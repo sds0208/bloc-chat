f8d1ebb093162b7fc18c60614b5cc95e0a998ff2
sds0208 <sarahsantacruz86@gmail.com>
2017-07-15 20:01:16 -0500

    working on getting rooms to display

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,9 @@
 (function() {
-    function HomeCtrl() {
+    function HomeCtrl(Room) {
+        this.allRooms = Room.all;
     }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', [HomeCtrl]);
+        .controller('HomeCtrl', ['Room', HomeCtrl]);
 })();
