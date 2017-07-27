5f7fec58bf0361813aa7d3d5767b2cbdfd6705c4
sds0208 <sarahsantacruz86@gmail.com>
2017-07-26 23:59:07 -0500

    still working on message display

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -1,12 +1,12 @@
 (function() {
     function Message($firebaseArray)  {
         var Message = {};
-        var ref = firebase.database().ref().child("messages").orderByChild("roomID");
+        var ref = firebase.database().ref().child("messages");
         var messages = $firebaseArray(ref);
 
 
         Message.getByRoomId = function(roomId)  {
-            return messages.equalTo(roomId);
+            return $firebaseArray(ref.orderByChild('roomID').equalTo(roomId));
         };
 
         return Message;
